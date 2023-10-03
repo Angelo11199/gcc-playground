edf462426951e0e0f857ebbeb04671e621ad51d3
Angelo11199 <142316765+Angelo11199@users.noreply.github.com>
2023-10-03 14:14:28 +0000

    init

--- table.cpp
+++ table.cpp
@@ -1,29 +1,97 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    int rows,columns;
-
-    for (rows = 1; rows <= 6; rows++)
-    {
-        
-        for (columns = 0; columns <= 6; columns++)
-        {
-            cout << "=" << "";
-            if(columns == 6)
-            {
-                cout << "||";
-            }
-        }
-                    if(rows == 1)
-            {
-                cout << "||";
-            }
-
-        
+int main() {
+    string data[9][8] = {{
+                             " | data 1 | ",
+                             " | data 2 | ",
+                             " | data 3 | ",
+                             " | data 4 | ",
+                             " | data 5 | ",
+                             " | data 6 | ",
+                             " | data 7 | ",
+                             " | data 8 | ",
+                         },
+                         {
+                             " | data d1 | ",
+                             " | data d2 | ",
+                             " | data d3 | ",
+                             " | data d4 | ",
+                             " | data d5 | ",
+                             " | data d6 | ",
+                             " | data d7 | ",
+                             " | data d8 | ",
+                         },
+                         {
+                             " | data c1 | ",
+                             " | data c2 | ",
+                             " | data c3 | ",
+                             " | data c4 | ",
+                             " | data c5 | ",
+                             " | data c6 | ",
+                             " | data c7 | ",
+                             " | data c8 | ",
+                         },
+                         {
+                             " | data b1 | ",
+                             " | data b2 | ",
+                             " | data b3 | ",
+                             " | data b4 | ",
+                             " | data b5 | ",
+                             " | data b6 | ",
+                             " | data b7 | ",
+                             " | data b8 | ",
+                         },
+                         {
+                             " | data a1 | ",
+                             " | data a2 | ",
+                             " | data a3 | ",
+                             " | data a4 | ",
+                             " | data a5 | ",
+                             " | data a6 | ",
+                             " | data a7 | ",
+                             " | data a8 | ",
+                         },
+                         {
+                             " | data d1 | ",
+                             " | data d2 | ",
+                             " | data d3 | ",
+                             " | data d4 | ",
+                             " | data d5 | ",
+                             " | data d6 | ",
+                             " | data d7 | ",
+                             " | data d8 | ",
+                         },
+                         {
+                             " | data d1 | ",
+                             " | data d2 | ",
+                             " | data d3 | ",
+                             " | data d4 | ",
+                             " | data d5 | ",
+                             " | data d6 | ",
+                             " | data d7 | ",
+                             " | data d8 | ",
+                         },
+                         {
+                             " | data d1 | ",
+                             " | data d2 | ",
+                             " | data d3 | ",
+                             " | data d4 | ",
+                             " | data d5 | ",
+                             " | data d6 | ",
+                             " | data d7 | ",
+                             " | data d8 | ",
+                         }};
+    int rows,
+        columns;  //* numbers
+    for (rows = 0; rows <= 5; rows++) {
         cout << "\n";
+        cout << "-------------------------------------------------------------------------------------------------------" << endl;
+        for (columns = 0; columns <= 7; columns++) {
+            cout << data[rows][columns];
+        }
     }
-    
-    
+    cout << "\n";
+    cout << "-------------------------------------------------------------------------------------------------------" << endl;
+
     return 0;
 }
