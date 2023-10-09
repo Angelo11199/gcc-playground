9088c9c453c10a76c799a4b4df0b994e12792ac3
Angelo11199 <142316765+Angelo11199@users.noreply.github.com>
2023-10-09 08:46:02 +0000

    fixes in module 1 file

--- m1/fun1.cpp
+++ m1/fun1.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int main(int argc, char const *argv[]) {
     // check if input is a  leaf year
     int year;
+
     cout << "Enter a year: ";
     cin >> year;
     if (year % 400 == 0) {
