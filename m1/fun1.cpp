d67eab99c1a271f62985df84c6e32b5780627090
Angelo11199 <142316765+Angelo11199@users.noreply.github.com>
2023-10-14 15:02:34 +0000

    Module 3 added

--- m1/fun1.cpp
+++ m1/fun1.cpp
@@ -1,36 +1,7 @@
-// include  math
-#include <stdio.h>
-
 #include <iostream>
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-    // check if input is a  leaf year
-    int year;
-
-    cout << "Enter a year: ";
-    cin >> year;
-    if (year % 400 == 0) {
-        cout << year << " is a leap year." << endl;
-    }
-    if (year % 100 == 0) {
-        if (year % 400 == 0) {
-            cout << year << " is a leap year." << endl;
-
-        } else {
-            cout << year << " is not a leap year." << endl;
-        }
-    }
-    if (year % 4 == 0) {
-        if (year % 100 == 0) {
-            cout << year << " is not a leap year." << endl;
-
-        } else {
-            cout << year << " is a leap year." << endl;
-        }
-    } else {
-        cout << year << " is not a leap year." << endl;
-    }
-
     return 0;
 }
+
