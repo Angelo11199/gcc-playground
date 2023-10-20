84e72d374dbd8312a20e7b6ef3bde8b609e4a964
Angelo11199 <142316765+Angelo11199@users.noreply.github.com>
2023-10-20 09:10:56 +0000

    Seatwork 2 added

---
+++ activities/main.cpp
@@ -0,0 +1,67 @@
+#include <math.h>
+
+#include <iostream>
+// include cstdllib
+#include <cstdlib>
+using namespace std;
+
+void print(string s) {
+    cout << s << endl;
+}
+int isPositive() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    int totalDigits = 0;
+    int temp = num;
+    while (temp != 0) {
+        temp /= 10;
+        totalDigits++;
+    }
+    if (totalDigits > 4) {
+        cout << "Number has more than 4 digits" << endl;
+        exit(0);
+    }
+    // check if num is positive
+    if (num > 0) {
+        cout << "Number has " << totalDigits << " digits" << endl;
+        cout << "Number is positive" << endl;
+    } else {
+        cout << "Number has " << totalDigits << " digits" << endl;
+        cout << "Number is non-positive number" << endl;
+    }
+    return 0;
+}
+int calculator() {
+    // create a calculator using switch case
+    int num1, num2;
+    char op;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    cout << "Enter operator: ";
+    cin >> op;
+    cout << "The result of the operation is : ";
+    switch (op) {
+        case '+':
+            cout << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << num1 * num2 << endl;
+            break;
+        case '/':
+            cout << num1 / num2 << endl;
+            break;
+        default:
+            cout << "Invalid operator" << endl;
+            break;
+    }
+    return 0;
+}
+int main(int argc, char const *argv[]) {
+    isPositive();
+}
