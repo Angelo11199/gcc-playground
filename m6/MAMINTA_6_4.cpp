35f5d4371588c37d6ba5ac733294eaebf9f2e16e
Angelo11199 <142316765+Angelo11199@users.noreply.github.com>
2023-10-29 11:23:58 +0000

    module 6 added

--- m6/MAMINTA_6_4.cpp
+++ m6/MAMINTA_6_4.cpp
@@ -6,9 +6,6 @@ int main(int argc, char const *argv[]) {
     print("add all numbers from 0 to n");
     int n = getNum("Enter n: ");
     int sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum = sum + i;
-        print(to_string(i));
-    }
+    for (int i = 0; i <= n; i++) sum += i;
     print("Sum is " + to_string(sum));
 }
