e2e6685ec699d7f0d6ec9fc17c9ae3b42b4b2084
Angelo11199 <142316765+Angelo11199@users.noreply.github.com>
2023-12-04 13:32:56 +0000

    Exam logic added

--- main/main.cpp
+++ main/main.cpp
@@ -5,7 +5,7 @@
 #include "../include/snippets.h"
 using namespace std;
 // max limit of factorial : 20. This is a recursive function
-long int getFactorial(long int n) {
+size_t getFactorial(size_t n) {
     do {
         if (n == 1 || n == 0) break;
         return n * getFactorial(n - 1);
@@ -19,7 +19,6 @@ void Reverse(string result, string* strPtr, int size) {
     }
 }
 void multiplication(int n, int max = 10) {
-    int n = -1;
     while (n <= 0) {
         n = getNum("Enter a number: ");
         if (n > 0) {
@@ -32,10 +31,46 @@ void multiplication(int n, int max = 10) {
         print(to_string(n) + " * " + to_string(i) + " = " + to_string(n * i));
 }
 int main(int argc, char const* argv[]) {
-    string value = getStr("Enter a string: ");
-    string result = "";
-    string* resultPtr = &result;
-    Reverse(value, resultPtr, value.length());
-    cout << "Reversed string: " << result << endl;
-    return 0;
+    bool isRunning = true;
+    while (isRunning) {
+        print("[F] Factorial");
+        print("[M] Multiplication");
+        print("[R] Reverse");
+        print("[Q] Quit");
+        char choice = tolower(getStr("Enter a choice: ")[0]);
+        // use switch case to call functions
+        switch (choice) {
+            case 'f': {
+                size_t n = getNum("Enter a number: ");
+                if (n > 20) {
+                    print("Number too large");
+                    break;
+                }
+                print("Factorial of " + to_string(n) + " is " + to_string(getFactorial(n)));
+                break;
+            }
+            case 'm': {
+                int n = getNum("Enter a number: ");
+                multiplication(n);
+                break;
+            }
+            case 'r': {
+                string value = getStr("Enter a string: ");
+                string result = "";
+                string* resultPtr = &result;
+                Reverse(value, resultPtr, value.length());
+                cout << "Reversed string: " << result << endl;
+                break;
+            }
+            case 'q': {
+                isRunning = false;
+                print("Quitting...");
+                break;
+            }
+            default: {
+                print("Invalid choice");
+                break;
+            }
+        }
+    }
 }
