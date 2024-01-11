e9e4f05e90d18d1537143ac96b59807a953d13e8
Angelo11199 <142316765+Angelo11199@users.noreply.github.com>
2024-01-11 09:44:07 +0000

    finished first formative assestment

--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,67 @@
+#include <ctime>
+
 #include "../include/snippets.h"
-int main(int argc, char const *argv[]) {
-    /* code */
+double* getNums() {
+    double* nums = new double[2];
+    nums[0] = getNum("Enter first number: ");
+    nums[1] = getNum("Enter second number: ");
+    return nums;
+}
+double Add(double a, double b) {
+    return a + b;
+}
+double Subtract(double a, double b) {
+    return a - b;
+}
+double Multiply(double a, double b) {
+    return a * b;
+}
+double Divide(double a, double b) {
+    return a / b;
+}
+
+int main(int argc, char const* argv[]) {
+    while (true) {
+        print("========================================");
+        print("                  MENU!               ");
+        print("========================================");
+        print("1. Add two numbers");
+        print("2. Subtract two numbers");
+        print("3. Multiply two numbers");
+        print("4. Divide two numbers");
+        print("5. Exit");
+        int choice = getNum("Enter your choice: ");
+        if (choice == 5) {
+            break;
+        }
+        double* nums = getNums();
+        switch (choice) {
+            case 1: {
+                printLn("Sum is: ");
+                print(Add(nums[0], nums[1]));
+
+                break;
+            }
+            case 2: {
+                print(Subtract(nums[0], nums[1]));
+
+                break;
+            }
+            case 3: {
+                print(Multiply(nums[0], nums[1]));
+                break;
+            }
+            case 4: {
+                print(Divide(nums[0], nums[1]));
+                break;
+            }
+            default: {
+                print("Invalid choice");
+                break;
+            }
+        }
+    }
+
+    // print(sum);
     return 0;
 }
